e78f58317dd15dfedc3fa831319d50eab9e0c3a4
Paul Bennett <pbennett.uk@outlook.com>
2026-07-22 19:29:31 +0100

    starship prompt fix

--- .config/shell/init.sh
+++ .config/shell/init.sh
@@ -2,5 +2,5 @@
 
 command -v mise >/dev/null 2>&1 && eval "$(mise activate bash)"
 command -v atuin >/dev/null 2>&1 && eval "$(atuin init bash)"
-command -v starship >/dev/null 2>&1 && eval "$(starship init bash)"
 command -v zoxide >/dev/null 2>&1 && eval "$(zoxide init bash)"
+command -v starship >/dev/null 2>&1 && eval "$(starship init bash)"
