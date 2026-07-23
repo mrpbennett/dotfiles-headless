d38e334a4293e3d63c20b28cdcf56ead3709d196
Paul Bennett <pbennett.uk@outlook.com>
2026-07-23 09:13:52 +0100

    tweaks

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,7 @@
 -- https://github.com/catppuccin/nvim
--- https://github.com/booberrytheme/boo-berry.nvim
--- https://github.com/Mofiqul/dracula.nvim
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = { colorscheme = "catppuccin-mocha" },
-  },
-  -- dracula
-  { "Mofiqul/dracula.nvim", lazy = true },
-  -- boo-berry
-  { "mrpbennett/boo-berry.nvim", lazy = true },
-  -- gruvbox
-  { "ellisonleao/gruvbox.nvim", lazy = true },
-  -- vault
-  { "mrpbennett/vault", lazy = true },
-  -- everforest
-  {
-    "sainnhe/everforest",
-    lazy = true,
-    config = function()
-      vim.g.everforest_background = "hard"
-      vim.g.everforest_better_performance = 1
-    end,
-  },
+    {
+        "LazyVim/LazyVim",
+        opts = { colorscheme = "catppuccin-macchiato" },
+    },
 }
--- .config/nvim/lua/plugins/default-tweaks/snacks.lua
+++ .config/nvim/lua/plugins/default-tweaks/snacks.lua
@@ -2,75 +2,72 @@
 --
 
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  keys = {
-    {
-      "<leader>.",
-      function()
-        require("utils.snacks.scratch").new_scratch({
-          { text = "go" },
-          { text = "html" },
-          { text = "javascript" },
-          { text = "python" },
-          { text = "sql" },
-          { text = "yaml" },
-        })
-      end,
-      desc = "New Scratch (pick filetype)",
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    keys = {
+        {
+            "<leader>.",
+            function()
+                require("utils.snacks.scratch").new_scratch({
+                    { text = "python" },
+                    { text = "yaml" },
+                    { text = "bash" },
+                })
+            end,
+            desc = "New Scratch (pick filetype)",
+        },
+        {
+            "<leader>S",
+            function()
+                require("utils.snacks.scratch").select_scratch()
+            end,
+            desc = "Select Scratch Buffer",
+        },
     },
-    {
-      "<leader>S",
-      function()
-        require("utils.snacks.scratch").select_scratch()
-      end,
-      desc = "Select Scratch Buffer",
-    },
-  },
-  opts = {
-    ---
-    dashboard = {
-      enabled = true,
-      example = "compact_files",
-      preset = {
-        header = [[
- __         ______     ______     __  __     __   __   __     __    __    
-/\ \       /\  __ \   /\___  \   /\ \_\ \   /\ \ / /  /\ \   /\ "-./  \   
-\ \ \____  \ \  __ \  \/_/  /__  \ \____ \  \ \ \'/   \ \ \  \ \ \-./\ \  
- \ \_____\  \ \_\ \_\   /\_____\  \/\_____\  \ \__|    \ \_\  \ \_\ \ \_\ 
-  \/_____/   \/_/\/_/   \/_____/   \/_____/   \/_/      \/_/   \/_/  \/_/ 
+    opts = {
+        ---
+        dashboard = {
+            enabled = true,
+            example = "compact_files",
+            preset = {
+                header = [[
+ __         ______     ______     __  __     __   __   __     __    __
+/\ \       /\  __ \   /\___  \   /\ \_\ \   /\ \ / /  /\ \   /\ "-./  \
+\ \ \____  \ \  __ \  \/_/  /__  \ \____ \  \ \ \'/   \ \ \  \ \ \-./\ \
+ \ \_____\  \ \_\ \_\   /\_____\  \/\_____\  \ \__|    \ \_\  \ \_\ \ \_\
+  \/_____/   \/_/\/_/   \/_____/   \/_____/   \/_/      \/_/   \/_/  \/_/
         ]],
-      },
-    },
-    ---
-    dim = {
-      enabled = true,
-    },
-    ---
-    image = {
-      enabled = true,
-    },
-    ---
-    picker = {
-      exclude = {
-        ".git",
-        ".venv",
-        ".DS_Store",
-      },
-      sources = {
-        explorer = {
-          ignored = true,
-          hidden = true,
+            },
         },
-        files = {
-          ignored = true,
-          hidden = true,
+        ---
+        dim = {
+            enabled = true,
         },
-      },
+        ---
+        image = {
+            enabled = true,
+        },
+        ---
+        picker = {
+            exclude = {
+                ".git",
+                ".venv",
+                ".DS_Store",
+            },
+            sources = {
+                explorer = {
+                    ignored = true,
+                    hidden = true,
+                },
+                files = {
+                    ignored = true,
+                    hidden = true,
+                },
+            },
+        },
+        ---
     },
     ---
-  },
-  ---
 }
