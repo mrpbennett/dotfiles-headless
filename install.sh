85a411d4be16ab6bf856b131e4b9a6b0a6ccb21c
Paul Bennett <pbennett.uk@outlook.com>
2026-07-23 13:23:48 +0100

    Change repository URL from dotfiles-headless to sdots

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-REPO_DIR="$HOME/.local/share/dotfiles-headless"
+REPO_DIR="$HOME/.local/share/sdots"
 
 DOTFILES_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]:-$REPO_DIR}")" 2>/dev/null && pwd || true)
 if [[ ! -d $DOTFILES_DIR/.git ]]; then
   DOTFILES_DIR=$REPO_DIR
-  [[ -d $DOTFILES_DIR/.git ]] || git clone --depth 1 https://github.com/mrpbennett/dotfiles-headless.git "$DOTFILES_DIR"
+  [[ -d $DOTFILES_DIR/.git ]] || git clone --depth 1 https://github.com/mrpbennett/sdots.git "$DOTFILES_DIR"
 fi
 
 sudo apt-get update
